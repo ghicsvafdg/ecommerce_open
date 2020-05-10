a5d71dbc3a27b1888f8f09c06146056a61f5a06e
phamhung21 <pham.vanhung.963871@gmail.com>
2020-05-10 15:48:19 +0700

    create migration

---
+++ database/migrations/2020_05_10_071826_create_database_table.php
@@ -0,0 +1,168 @@
+<?php
+
+use Illuminate\Support\Facades\Schema;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Support\Facades\DB;
+
+class CreateDatabaseTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+
+        Schema::create('product_categories', function (Blueprint $table) {
+            $table->increments('id');
+            $table->string('name');
+            $table->string('slug');
+            $table->timestamp('created_at')->default(DB::raw('CURRENT_TIMESTAMP'));
+            $table->timestamp('updated_at')->default(DB::raw('CURRENT_TIMESTAMP'));
+        });
+
+        Schema::create('tags', function (Blueprint $table) {
+            $table->increments('id');
+            $table->string('name');
+           
+            $table->timestamp('created_at')->default(DB::raw('CURRENT_TIMESTAMP'));
+            $table->timestamp('updated_at')->default(DB::raw('CURRENT_TIMESTAMP'));
+        });
+
+        Schema::create('products', function(Blueprint $table){
+
+            $table->increments('id')->unique();
+
+            $table->unsignedInteger('category_id');
+            $table->foreign('category_id')->references('id')->on('product_categories')->onDelete('cascade');
+
+            $table->unsignedInteger('tag_id');
+            $table->foreign('tag_id')->references('id')->on('tags')->onDelete('cascade');
+
+            $table->longText('name');
+            $table->decimal('price');
+            $table->mediumText('image');
+            $table->integer('quantity');
+            $table->string('color')->nullable();
+            $table->string('size')->nullable();
+            $table->decimal('promotion')->nullable();
+            $table->longText('description');
+            $table->longText('detail');
+            
+            $table->timestamp('created_at')->default(DB::raw('CURRENT_TIMESTAMP'));
+            $table->timestamp('updated_at')->default(DB::raw('CURRENT_TIMESTAMP'));
+        });
+
+        Schema::create('users', function (Blueprint $table) {
+            $table->increments('id');
+            $table->string('name');
+            $table->string('username');
+            $table->integer('role')->default('1');
+            $table->string('password');
+            $table->string('address')->nullable();
+            $table->string('phone')->nullable();
+            $table->string('dob')->nullable();
+            $table->string('email')->unique();
+           
+            $table->rememberToken();
+            $table->timestamp('created_at')->default(DB::raw('CURRENT_TIMESTAMP'));
+            $table->timestamp('updated_at')->default(DB::raw('CURRENT_TIMESTAMP'));
+        });
+
+        Schema::create('vouchers', function(Blueprint $table){
+
+            $table->increments('id')->unique();
+
+            $table->string('code');
+            
+            $table->unsignedInteger('product_id');
+            $table->foreign('product_id')->references('id')->on('products')->onDelete('cascade');
+
+            $table->integer('times_use');
+            $table->integer('value');
+            
+            $table->timestamp('created_at')->default(DB::raw('CURRENT_TIMESTAMP'));
+            $table->timestamp('updated_at')->default(DB::raw('CURRENT_TIMESTAMP'));
+        });
+
+        Schema::create('posts', function(Blueprint $table){
+
+            $table->increments('id')->unique();
+
+            $table->string('title');
+            $table->string('slug');
+            $table->integer('description');
+            $table->integer('content');
+            $table->string('image');
+            
+            $table->timestamp('created_at')->default(DB::raw('CURRENT_TIMESTAMP'));
+            $table->timestamp('updated_at')->default(DB::raw('CURRENT_TIMESTAMP'));
+        });
+
+        Schema::create('carts', function(Blueprint $table){
+
+            $table->increments('id')->unique();
+
+            $table->unsignedInteger('product_id');
+            $table->foreign('product_id')->references('id')->on('products')->onDelete('cascade');
+
+            $table->unsignedInteger('user_id');
+            $table->foreign('user_id')->references('id')->on('users')->onDelete('cascade');
+            
+            $table->timestamp('created_at')->default(DB::raw('CURRENT_TIMESTAMP'));
+            $table->timestamp('updated_at')->default(DB::raw('CURRENT_TIMESTAMP'));
+        });
+
+        Schema::create('payments', function(Blueprint $table){
+
+            $table->increments('id')->unique();
+
+            $table->unsignedInteger('product_id');
+            $table->foreign('product_id')->references('id')->on('products')->onDelete('cascade');
+
+            $table->unsignedInteger('user_id');
+            $table->foreign('user_id')->references('id')->on('users')->onDelete('cascade');
+            
+            $table->integer('method');
+
+            $table->timestamp('created_at')->default(DB::raw('CURRENT_TIMESTAMP'));
+            $table->timestamp('updated_at')->default(DB::raw('CURRENT_TIMESTAMP'));
+        });
+
+        Schema::create('orders', function(Blueprint $table){
+
+            $table->increments('id')->unique();
+
+            $table->string('code');
+
+            $table->unsignedInteger('product_id');
+            $table->foreign('product_id')->references('id')->on('products')->onDelete('cascade');
+
+            $table->unsignedInteger('user_id');
+            $table->foreign('user_id')->references('id')->on('users')->onDelete('cascade');
+            
+            $table->timestamp('created_at')->default(DB::raw('CURRENT_TIMESTAMP'));
+            $table->timestamp('updated_at')->default(DB::raw('CURRENT_TIMESTAMP'));
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('product_categories');
+        Schema::dropIfExists('tags');
+        Schema::dropIfExists('products');
+        Schema::dropIfExists('users');
+        Schema::dropIfExists('vouchers');
+        Schema::dropIfExists('posts');
+        Schema::dropIfExists('carts');
+        Schema::dropIfExists('payments');
+        Schema::dropIfExists('orders');
+    }
+}
