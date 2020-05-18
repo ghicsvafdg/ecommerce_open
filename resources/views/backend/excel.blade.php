84261e0a120bb1c293933c39e9f25a56249b5fca
phamhung2109 <pham.vanhung.963871@gmail.com>
2020-05-18 13:59:32 +0700

    validate file when import data

--- resources/views/backend/excel.blade.php
+++ resources/views/backend/excel.blade.php
@@ -13,47 +13,26 @@
     <div class="container">
         <h3 align="center">Import Excel File in Laravel</h3>
         <br />
-        @if(count($errors) > 0)
-        <div class="alert alert-danger">
-            Upload Validation Error<br><br>
-            <ul>
-                @foreach($errors->all() as $error)
-                <li>{{ $error }}</li>
-                @endforeach
-            </ul>
+        @if (\Session::has('msg'))
+        <div class="alert alert-danger" style="margin-left: 10px">
+            <h5> {!! \Session::get('msg') !!}</h5>
+        </div>
+        @elseif($errors->any())
+        <div class="alert alert-danger" style="margin-left: 10px">
+            <p>Thêm file thất bại, vui lòng kiểm tra lại định dạng hoặc dữ liệu file</p>
         </div>
         @endif
         
-        @if($message = Session::get('success'))
-        <div class="alert alert-success alert-block">
-            <button type="button" class="close" data-dismiss="alert">×</button>
-            <strong>{{ $message }}</strong>
-        </div>
-        @endif
         <form method="post" enctype="multipart/form-data" action="{{ url('/import_excel/import') }}">
             {{ csrf_field() }}
             <div class="form-group">
                 <table class="table">
-                    {{-- <tr>
-                        <td width="40%" align="right"><label>Select File for Upload</label></td>
-                        <td width="30">
-                            <input type="file" name="select_file" />
-                        </td>
-                        <td width="30%" align="left">
-                            <input type="submit" name="upload" class="btn btn-primary" value="Upload">
-                        </td>
-                        <button class="btn btn-success">Import User Data</button>
-                        <a class="btn btn-warning" href="{{ route('export') }}">Export User Data</a>
-                    </tr>
-                    <tr>
-                        <td width="40%" align="right"></td>
-                        <td width="30"><span class="text-muted">.xls, .xslx</span></td>
-                        <td width="30%" align="left"></td>
-                    </tr> --}}
-                    <input type="file" name="file" class="form-control">
+                   
+                    <input type="file" name="file" class="form-control" required>
                     <br>
                     <button class="btn btn-success">Import User Data</button>
                     <a class="btn btn-warning" href="{{ route('export') }}">Export User Data</a>
+                    <a class="btn btn-primary" href="{{route('manage-product.index')}}">Quay về</a>
                 </table>
             </div>
         </form>
